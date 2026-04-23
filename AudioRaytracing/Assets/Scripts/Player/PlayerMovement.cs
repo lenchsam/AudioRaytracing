d85656d72e5e6295c100f169f9fcda0283659a62
Sam <114984800+lenchsam@users.noreply.github.com>
2026-04-23 12:01:51 +0100

    Player controller and file organisation

---
+++ AudioRaytracing/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,76 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("Movement")]
+    [SerializeField] float moveSpeed = 5f;
+    [SerializeField] float jumpHeight = 2f;
+    [SerializeField] float gravity = -9.81f;
+
+    [Header("Look")]
+    [SerializeField] float mouseSensitivity = 0.1f;
+    [SerializeField] Transform cameraTransform;
+
+    private CharacterController controller;
+    private InputSystem_Actions controls;
+    private Vector2 moveInput;
+    private Vector2 lookInput;
+    private Vector3 velocity;
+    private float xRotation = 0f;
+
+    void Awake()
+    {
+        controller = GetComponent<CharacterController>();
+        controls = new InputSystem_Actions();
+
+        controls.Player.Jump.performed += ctx => Jump();
+    }
+    void OnEnable() {
+        controls.Enable();
+    }
+    void OnDisable(){
+        controls.Disable();
+    }
+
+    void Update()
+    {
+        moveInput = controls.Player.Move.ReadValue<Vector2>();
+        lookInput = controls.Player.Look.ReadValue<Vector2>();
+
+        HandleLook();
+        HandleMovement();
+    }
+
+    void HandleMovement()
+    {
+        if (controller.isGrounded && velocity.y < 0)
+            velocity.y = -2f;
+
+        Vector3 move = transform.right * moveInput.x + transform.forward * moveInput.y;
+        controller.Move(move * moveSpeed * Time.deltaTime);
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+
+    void HandleLook()
+    {
+        float mouseX = lookInput.x * mouseSensitivity;
+        float mouseY = lookInput.y * mouseSensitivity;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        cameraTransform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        transform.Rotate(Vector3.up * mouseX);
+    }
+
+    void Jump()
+    {
+        if (controller.isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+    }
+}
