abeb837657e50dd9eface783157413e17ee04115
Sam <114984800+lenchsam@users.noreply.github.com>
2026-07-16 17:34:31 +0100

    change song in game

--- AudioRaytracing/Assets/Scripts/Button.cs
+++ AudioRaytracing/Assets/Scripts/Button.cs
@@ -3,12 +3,23 @@ using UnityEngine.InputSystem;
 
 public class Button : MonoBehaviour
 {
+    enum ButtonType
+    {
+        ToggleEnvironment,
+        ChangeSong
+    }
+
     private InputSystem_Actions controls;
 
     [SerializeField] private AudioRaytracer m_audioRaytracer;
 
     [SerializeField] private GameObject m_button;
     [SerializeField] private GameObject m_goToToggle;
+
+    [SerializeField] private ButtonType m_buttonType;
+    [SerializeField] private AudioClip[] m_audioClip;
+    [SerializeField] private AudioSource m_audioSource;
+    private int m_audioClipIndex = 0;
 
     private void Awake()
     {
@@ -36,8 +47,29 @@ public class Button : MonoBehaviour
 
         if (Physics.Raycast(ray, out RaycastHit hit) && hit.collider.gameObject == m_button)
         {
-            m_goToToggle.SetActive(!m_goToToggle.activeSelf);
-            m_audioRaytracer.RebuildCubeBuffer();
+            switch(m_buttonType)
+            {
+                case ButtonType.ToggleEnvironment:
+                    ToggleEnvironment();
+                    break;
+                case ButtonType.ChangeSong:
+                    RotateSong();
+                    break;
+            }
         }
+    }
+
+    private void ToggleEnvironment()
+    {
+        m_goToToggle.SetActive(!m_goToToggle.activeSelf);
+        m_audioRaytracer.RebuildCubeBuffer();
+    }
+    private void RotateSong()
+    {
+        AudioClip currentAudioClip = m_audioClip[m_audioClipIndex];
+
+        m_audioSource.transform.GetChild(0).GetComponent<AudioSource>().clip = currentAudioClip;
+        m_audioSource.transform.GetChild(0).GetComponent<AudioSource>().Play();
+        m_audioClipIndex = (m_audioClipIndex + 1) % m_audioClip.Length;
     }
 }
